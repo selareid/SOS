4911d5bdeb2fa4d6e159f8a1712b72b64034db43
Daniel <danielselareid40@gmail.com>
2017-06-14 11:00:01 +0700

    Patched strgDistr
    Typo

--- Processes.js
+++ Processes.js
@@ -483,7 +483,7 @@ module.exports = {
 
             var flag = room.find(FIND_FLAGS, {filter: (f) => f.name.split(':')[0] == 'distrSquare'})[0];
             if (!room.storage || !flag) return;
-            if (!flag.findInRange(FIND_STRUCTURES, 1, {filter: (s) => s.structureType == STRUCTURE_LINK})[0]) return this.placeStrucs(room, flag);
+            if (!flag.pos.findInRange(FIND_STRUCTURES, 1, {filter: (s) => s.structureType == STRUCTURE_LINK})[0]) return this.placeStrucs(room, flag);
             if (Game.time % 1001 == 0) this.placeStrucs(room, flag);
 
             if (creeps.length > 0) {
