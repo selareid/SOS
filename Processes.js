847e04085bbcf85c752f903b8231a0ddfbbc5317
Daniel Reid <danielselareid40@gmail.com>
2017-06-19 17:41:27 +0700

    don’t need that stuff

--- Processes.js
+++ Processes.js
@@ -373,13 +373,8 @@ module.exports = {
 
             if (!storageLink) return global[room.name].links = undefined;
 
-            var energySent = storageLink.energy;
-
             _.forEach(sourceLinks, (l) => {
-                if (l.cooldown == 0 && l.energy >= l.energyCapacity && energySent < storageLink.energyCapacity) {
-                    l.transferEnergy(storageLink);
-                    energySent =+ l.energy;
-                }
+                if (l.cooldown == 0 && l.energy >= l.energyCapacity) l.transferEnergy(storageLink);
             });
             
             
