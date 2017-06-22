ed4e867d00892bbf6e668eed4bdc41c31415e8d8
Daniel Reid <danielselareid40@gmail.com>
2017-06-22 15:43:38 +0700

    ?

--- Processes.js
+++ Processes.js
@@ -401,7 +401,7 @@ module.exports = {
             if (!room || !room.storage) return 'end';
             if (!global[room.name]) global[room.name] = {};
             if (!creeps) return Memory.crps = [];
-            if (!Memory.tasks) return Memory.tasks = [];
+            if (!Memory.tasks && !typeof Memory.tasks == 'array') return Memory.tasks = [];
             if (!Memory.ext || Game.time % 15013) return Memory.ext = _.map(room.find(FIND_MY_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_EXTENSION}), (s) => {return s.id})
 
             if (room.controller.ticksToDowngrade <= 2000 && !Memory.tasks.includes('fixController')) Memory.tasks.push('fixController');
