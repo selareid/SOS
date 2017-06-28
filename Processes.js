f51029800fceafead27a9ebcdbc87502364523a6
Daniel <danielselareid40@gmail.com>
2017-06-28 10:53:45 +0700

    Update Processes.js

--- Processes.js
+++ Processes.js
@@ -519,7 +519,7 @@ module.exports = {
                 }
 
                 if (room.terminal.store[RESOURCE_ENERGY] >= 75000 && room.storage.store[RESOURCE_ENERGY] >= 50000) {
-                    var order = _.max(Game.market.getAllOrders({resourceType: RESOURCE_ENERGY, type: ORDER_BUY}), (o) => o.price / Game.market.calcTransactionCost(10, room.name, o.roomName));
+                    var order = _.max(Game.market.getAllOrders({resourceType: RESOURCE_ENERGY, type: ORDER_BUY}), (o) => {if (o.amount >= 10) return o.price / Game.market.calcTransactionCost(10, room.name, o.roomName)});
                     if (!order) return;
 
                     var engRsl = Game.market.deal(order.id, (24000 > order.amount ? order.amount : 24000), room.name);
