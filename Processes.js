2727cd23d840485472abc8538f39d0a5749b58ac
Daniel Reid <danielselareid40@gmail.com>
2017-07-16 14:49:34 +0700

    shrug

--- Processes.js
+++ Processes.js
@@ -354,7 +354,7 @@ module.exports = {
             var Memory = global.Mem.p[Memory_it];
 
             var nextToSpawn = _.sortBy(Memory.spawnQueue, (c) => {
-                return c.proc == 'doHarvest' ? 0 : c.proc == 'fillSpawn' ? 1 : 2;
+                return c.proc == 'doHarvest' ? 0 : c.proc == 'fillSpawn' ? 1 : c.proc == 'claim' ? 2 : 3;
             })[0];
 
             if (!nextToSpawn) return;
