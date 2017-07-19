0fbf2e558421a9a7fd99b881a345033fd1967640
Daniel Reid <danielselareid40@gmail.com>
2017-07-19 11:36:32 +0700

    damn bug

--- Processes.js
+++ Processes.js
@@ -499,7 +499,7 @@ module.exports = {
                     if (structs.length > 0) continue;
 
                     let flags = pos.findInRange(FIND_FLAGS, 4);
-                    if (structs.length > 0) continue;
+                    if (flags.length > 0) continue;
 
                     let terrain = _.filter(room.lookForAtArea(LOOK_TERRAIN, y - freeRange, x - freeRange, y + freeRange, x + freeRange, true), (p) => p.type == 'terrain' && p.terrain == 'wall');
                     if (terrain.length > 0) continue;
