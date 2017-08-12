b7097671ae41d1db214472485ecc70ca7835046e
Daniel Reid <danielselareid40@gmail.com>
2017-08-12 12:48:28 +0700

    Cry

--- pathing.js
+++ pathing.js
@@ -3,34 +3,30 @@ Creep.prototype.moveWithPath =
         try {
             if (dest.pos) dest = dest.pos;
 
-            if (dest && dest.roomName == this.pos.roomName) {
-                if (!global[this.room.name]) global[this.room.name] = {};
-                if (!global[this.room.name].paths) global[this.room.name].paths = {};
+            if (!global[this.room.name]) global[this.room.name] = {};
+            if (!global[this.room.name].paths) global[this.room.name].paths = {};
 
-                var thisPosName = (this.pos.x * 100 + this.pos.y).toString(36);
-                var destPosName = (dest.x * 100 + dest.y).toString(36);
+            var thisPosName = (this.pos.x * 100 + this.pos.y).toString(36);
+            var destPosName = (dest.x * 100 + dest.y).toString(36);
+            var roomTag = this.pos.roomName + dest.roomName;
 
-                if (this.memory.path && this.memory.path.split(',')[1] == destPosName) thisPosName = this.memory.path.split(',')[1];
+            if (this.memory.path && this.memory.path.split(',')[2] == destPosName) thisPosName = this.memory.path.split(',')[1];
 
-                if (global[this.room.name].paths[thisPosName + ',' + destPosName]) {
-                    var rsl = this.moveByPath(global[this.room.name].paths[thisPosName + ',' + destPosName]);
-                    this.memory.goto++;
+            if (global[this.room.name].paths[roomTag + ',' + thisPosName + ',' + destPosName]) {
+                var rsl = this.moveByPath(global[this.room.name].paths[roomTag + ',' + thisPosName + ',' + destPosName]);
+                this.memory.goto++;
 
-                    if (rsl == ERR_NOT_FOUND || !global[this.room.name].paths[thisPosName + ',' + destPosName][this.memory.goto + 1]) {
-                        delete this.memory.goto;
-                        delete this.memory.path;
-                    }
-                }
-                else {
-                    global[this.room.name].paths[thisPosName + ',' + destPosName] = this.pos.findPathTo(dest, opts);
-
-                    this.moveByPath(global[this.room.name].paths[thisPosName + ',' + destPosName]);
-                    this.memory.goto = 1;
-                    this.memory.path = thisPosName + ',' + destPosName;
+                if (rsl == ERR_NOT_FOUND || !global[this.room.name].paths[roomTag + ',' + thisPosName + ',' + destPosName][this.memory.goto + 1]) {
+                    delete this.memory.goto;
+                    delete this.memory.path;
                 }
             }
-            else if (dest) {
-                this.travelTo(dest, opts);
+            else {
+                global[this.room.name].paths[roomTag + ',' + thisPosName + ',' + destPosName] = this.pos.findPathTo(dest, opts);
+
+                this.moveByPath(global[this.room.name].paths[roomTag + ',' + thisPosName + ',' + destPosName]);
+                this.memory.goto = 1;
+                this.memory.path = roomTag + ',' + thisPosName + ',' + destPosName;
             }
         }
         catch (err) {
