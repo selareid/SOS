7cb467359ef7b11fea08b9a35db7cf67ca347444
Daniel Reid <danielselareid40@gmail.com>
2017-08-12 18:27:41 +0700

    DO MORE!

--- Processes.js
+++ Processes.js
@@ -1205,7 +1205,7 @@ module.exports = {
                     }
             }
 
-            return {response: 'idle', time: Game.time + (24 + (Math.round(Math.random() * 15)))};
+            return {response: 'idle', time: Game.time + (15 + (Math.round(Math.random() * 15)))};
         }
     },
 
