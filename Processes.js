298a3feeb0d47d378df1be9e58862a829a2553d2
Daniel Reid <danielselareid40@gmail.com>
2017-08-16 16:55:50 +0700

    fx

--- Processes.js
+++ Processes.js
@@ -1375,7 +1375,7 @@ module.exports = {
                     }
 
                     if (!creep) {
-                        if (!global.Mem.p['room:' + room.name].spawnQueue[creeps[creep_it_it]]) creeps.splice(creep_it_it, 1);
+                        if (!room.memory.spawnQueue[creeps[creep_it_it]]) creeps.splice(creep_it_it, 1);
                         continue;
                     }
 
