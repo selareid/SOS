d86a534f46eee22ab01a06a12f4a14b65bfa24e6
Daniel Reid <danielselareid40@gmail.com>
2017-08-21 21:19:36 +0700

    Revert "Fixes"
    This reverts commit 2e13c2f533a99b2a5413ea4c4a4b606e481149ed.

--- prototype.room-position.js
+++ prototype.room-position.js
@@ -2,33 +2,27 @@ RoomPosition.prototype.findNearbyStructure = function (structureType, filter) {
     if (!this || !this.roomName || !Game.rooms[this.roomName]) return [];
 
     var room = Game.rooms[this.roomName];
-    var found;
 
-    found = _.filter(room.lookForAt(LOOK_STRUCTURES, this), (s) => s.structureType == structureType && (!filter || !filter instanceof Function || (s)))
+    var founds = [room.lookForAt(LOOK_STRUCTURES, this),
+        room.lookForAt(LOOK_STRUCTURES, this.x, this.y-1),
+        room.lookForAt(LOOK_STRUCTURES, this.x, this.y+1),
+        room.lookForAt(LOOK_STRUCTURES, this.x-1, this.y),
+        room.lookForAt(LOOK_STRUCTURES, this.x+1, this.y),
+        room.lookForAt(LOOK_STRUCTURES, this.x-1, this.y-1),
+        room.lookForAt(LOOK_STRUCTURES, this.x+1, this.y-1),
+        room.lookForAt(LOOK_STRUCTURES, this.x+1, this.y+1),
+        room.lookForAt(LOOK_STRUCTURES, this.x-1, this.y+1)];
 
-    found = _.filter(room.lookForAt(LOOK_STRUCTURES, this.x, this.y - 1), (s) => s.structureType == structureType && (!filter || !filter instanceof Function || (s)));
-    if (found && found.length > 0) return found;
+    var structures = [];
 
-    found = _.filter(room.lookForAt(LOOK_STRUCTURES, this.x, this.y + 1), (s) => s.structureType == structureType && (!filter || !filter instanceof Function || (s)));
-    if (found && found.length > 0) return found;
+    for (let found of founds) {
+        if (!found) continue;
 
-    found = _.filter(room.lookForAt(LOOK_STRUCTURES, this.x - 1, this.y), (s) => s.structureType == structureType && (!filter || !filter instanceof Function || (s)));
-    if (found && found.length > 0) return found;
+        var structure;
+        structure = _.filter(found, (s) => s.structureType == structureType && (!filter || !filter instanceof Function || (s)))[0];
 
-    found = _.filter(room.lookForAt(LOOK_STRUCTURES, this.x + 1, this.y), (s) => s.structureType == structureType && (!filter || !filter instanceof Function || (s)));
-    if (found && found.length > 0) return found;
+        if (structure) structures.push(structure);
+    }
 
-    found = _.filter(room.lookForAt(LOOK_STRUCTURES, this.x - 1, this.y - 1), (s) => s.structureType == structureType && (!filter || !filter instanceof Function || (s)));
-    if (found && found.length > 0) return found;
-
-    found = _.filter(room.lookForAt(LOOK_STRUCTURES, this.x + 1, this.y - 1), (s) => s.structureType == structureType && (!filter || !filter instanceof Function || (s)));
-    if (found && found.length > 0) return found;
-
-    found = _.filter(room.lookForAt(LOOK_STRUCTURES, this.x + 1, this.y + 1), (s) => s.structureType == structureType && (!filter || !filter instanceof Function || (s)));
-    if (found && found.length > 0) return found;
-
-    found = _.filter(room.lookForAt(LOOK_STRUCTURES, this.x - 1, this.y + 1), (s) => s.structureType == structureType && (!filter || !filter instanceof Function || (s)));
-    if (found && found.length > 0) return found;
-
-    return [];
+    return structures;
 };
