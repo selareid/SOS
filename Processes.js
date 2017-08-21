1659a4eaaf36000d2bb1d19e7b9f5b10864f5a90
Daniel Reid <danielselareid40@gmail.com>
2017-08-21 17:17:52 +0700

    Better

--- Processes.js
+++ Processes.js
@@ -1093,7 +1093,10 @@ module.exports = {
                 for (var orderIndex in global.Mem.market) {
                     var order = Game.market.getOrderById(global.Mem.market[orderIndex]);
 
-                    if (!order || order.amount < 10) continue;
+                    if (!order || order.amount < 10) {
+                        delete global.Mem.market[orderIndex];
+                        continue;
+                    }
 
                     switch (order.type) {
                         case ORDER_SELL: //you buy
