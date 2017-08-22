6e4542cd80f7f5af685238eb1b56903518d7ab0d
Daniel <danielselareid40@gmail.com>
2017-08-22 13:07:45 +0700

    Update prototype.room-position.js

--- prototype.room-position.js
+++ prototype.room-position.js
@@ -1,5 +1,5 @@
 RoomPosition.prototype.findNearbyStructure = function (structureType, filter) {
-    if (!Game.rooms[this.roomName]) return [];
+    if (!Game.rooms[this.roomName] || structureType == STRUCTURE_EXTENSION) return [];
     
     return this.findInRange(Game.rooms[this.roomName].getStructures(structureType), 1, (filter && filter instanceof Function ? filter : undefined));
 };
