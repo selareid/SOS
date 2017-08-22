14dbc8b5bae136e2546b4b519e4d44d6c520c808
Daniel <danielselareid40@gmail.com>
2017-08-22 09:35:19 +0700

    Update prototype.room-position.js

--- prototype.room-position.js
+++ prototype.room-position.js
@@ -1,5 +1,5 @@
 RoomPosition.prototype.findNearbyStructure = function (structureType, filter) {
     if (!this.room) return [];
     
-    return this.findInRange(this.room.getStructures(structureType), 1, filter);
+    return this.findInRange(this.room.getStructures(structureType), 1, (filter && filter instanceof Function ? filter : undefined));
 };
