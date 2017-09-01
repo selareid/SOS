46779c96c311338c3c64ac3db3e3c439f882a6cb
Daniel Reid <danielselareid40@gmail.com>
2017-09-01 16:46:56 +0700

    buying that power, power, power, power

--- Processes.js
+++ Processes.js
@@ -1199,9 +1199,14 @@ module.exports = {
 
             var powerSpawn = room.getStructures(STRUCTURE_POWER_SPAWN)[0];
 
-            if (powerSpawn && powerSpawn.energy >= 50 && powerSpawn.power >= 1) powerSpawn.processPower();
+            if (powerSpawn && powerSpawn.energy >= 50 && powerSpawn.power >= 1 && room.storage && room.storage.store[RESOURCE_ENERGY] >= 50000) powerSpawn.processPower();
+            else if (powerSpawn.power == 0 && room.terminal && Memory.market && Memory.market['power_buy']
+                && room.terminal.cooldown && room.terminal.store[RESOURCE_ENERGY] >= 10000 && Game.market.credits > 10000 && room.storage) {
+                Game.market.deal(Memory.market['power_buy'], 100, room.name);
+                return {response: 'idle', time: Game.time + 101};
+            }
 
-            return {response: 'idle', time: Game.time + 3 + Math.round(Math.random() * 2)};
+            return {response: 'idle', time: Game.time + 7};
         }
     },
 
