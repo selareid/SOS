67c9345e64f3176bbcb09920d343181b7fd86b8b
Daniel Reid <danielselareid40@gmail.com>
2017-09-23 15:32:51 +0700

    double ext when storage full

--- Processes.js
+++ Processes.js
@@ -1562,7 +1562,7 @@ module.exports = {
 
         getCreepAmount: function (room) {
             if (!room || !room.controller || room.controller.level >= 7) return 1;
-            else return 2;
+            else return room.controller.level < 8 && room.storage.store.energy > 200000 ? 4 : 2;
         },
 
         notCoolVersion: function (Memory, room, creep) {
