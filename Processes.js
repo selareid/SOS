ba0ae15c75828db264735660a5b163cab067d31a
Daniel <danielselareid40@gmail.com>
2017-09-26 18:03:33 +0700

    better

--- Processes.js
+++ Processes.js
@@ -2099,10 +2099,7 @@ module.exports = {
                         else creep.moveWithPath(room.controller, {repath: 0.01, maxRooms: 1});
                     }
                     else {
-                        if (creep.pos.getRangeTo(room.controller) > 1) creep.moveWithPath(room.controller, {range: 1, repath: 0.01, maxRooms: 1, ignoreCreeps: false});
-                        else {
-                            creep.upgradeController(room.controller);
-                        }
+                        if (creep.upgradeController(room.controller) == ERR_NOT_IN_RANGE) creep.moveWithPath(room.controller, {range: 1, repath: 0.01, maxRooms: 1, ignoreCreeps: false});
                     }
 
                 }
