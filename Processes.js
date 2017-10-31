32a27afd36e17baeae8b0a25364d9e1f2dac033b
Daniel <danielselareid40@gmail.com>
2017-10-31 07:40:40 +0700

    Update Processes.js

--- Processes.js
+++ Processes.js
@@ -565,7 +565,7 @@ module.exports = {
             var room = Game.rooms[Memory.rmN];
 
             var nextToSpawn = _.sortBy(room.memory.spawnQueue, (c) => {
-                return c.body.cost > room.energyCapacityAvailable ? 10: c.proc == 'doHarvest' ? 0 : c.proc == 'fillSpawn' ? 1 : c.proc == 'strgDistr' ? 2 : c.proc == 'fillExt' ? 3 : c.proc == 'claim' ? 4 : c.proc == 'praiseRC' ? 6 : 5;
+                return c.proc == 'doHarvest' ? 0 : c.proc == 'fillSpawn' ? 1 : c.proc == 'strgDistr' ? 2 : c.proc == 'fillExt' ? 3 : c.proc == 'claim' ? 4 : c.proc == 'praiseRC' ? 6 : 5;
             })[0];
 
             if (!nextToSpawn) return;
