c6f0c87452c91ce04cdd32f33ec4907390fda4c4
Daniel <danielselareid40@gmail.com>
2017-11-08 12:59:05 +0700

    Update Processes.js

--- Processes.js
+++ Processes.js
@@ -588,7 +588,7 @@ return;
                         if (room.terminal && !processExists('doTerminal', Memory.rmN)) spawnNewProcess('doTerminal', Memory.rmN);
 
                         var mineral = room.find(FIND_MINERALS)[0];
-                        if (mineral && ((mineral.mineralAmount > 1 || mineral.ticksToRegeneration < 200) && room.getStructures(STRUCTURE_EXTRACTOR).length > 0 && !processExists('mine', Memory.rmN))) spawnNewProcess('mine', Memory.rmN);
+                        if (mineral && ((mineral.mineralAmount > 1 || mineral.ticksToRegeneration < 200) && !processExists('mine', Memory.rmN))) spawnNewProcess('mine', Memory.rmN);
                     }
 
                     if (!processExists('doHarvest', Memory.rmN)) spawnNewProcess('doHarvest', Memory.rmN);
