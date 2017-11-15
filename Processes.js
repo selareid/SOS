0a42a8eb5206f7cf52c215301c57fed74acd577d
Daniel Reid <danielselareid40@gmail.com>
2017-11-15 09:59:06 +0700

    trynna fix it up

--- Processes.js
+++ Processes.js
@@ -1926,7 +1926,7 @@ return;
                     if (!room.terminal.store[RESOURCE_ENERGY]) continue;
                     
                     if (resourceType == RESOURCE_ENERGY) {
-                        if (room.storage.store[RESOURCE_ENERGY]+800 < 900000 || room.storage.store[resourceType]+800 < storageEnergy) {
+                        if ((room.terminal.store[RESOURCE_ENERGY]-800 > terminalGoals[RESOURCE_ENERGY] && room.storage.store[RESOURCE_ENERGY]+800 < 900000) || room.storage.store[resourceType]+800 < storageEnergy) {
                             resourceToMove = resourceType;
                             break;
                         }
