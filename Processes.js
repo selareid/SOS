5e26e3084f5f420545bb9b0695b7d3a4e76185a4
Daniel <danielselareid40@gmail.com>
2017-11-30 15:50:41 +0700

    fixes

--- Processes.js
+++ Processes.js
@@ -2122,7 +2122,7 @@ return;
                         }
                     }
                     else if (room.storage.store[resourceType]-500 > 0 && (!room.terminal.store[resourceType] || room.terminal.store[resourceType]+800 < terminalGoals[resourceType])) {
-                        amountToMove = room.storage.store[resourceType]-500 > 0 ? room.storage.store[resourceType]-500 : undefined;
+                        amountToMove = room.storage.store[resourceType]-500 < creep.carryCapacity-_.sum(creep.carry) ? room.storage.store[resourceType]-500 : undefined;
                         resourceToMove = resourceType;
                         break;
                     }
