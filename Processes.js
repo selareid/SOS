6283f12c8a6337881b10838c22f42a58fb26d6a0
Daniel Reid <danielselareid40@gmail.com>
2017-12-05 19:10:20 +0700

    fixed look coords out of bounds

--- Processes.js
+++ Processes.js
@@ -1308,8 +1308,8 @@ return;
             const freeRange = 5;
             var bestPos;
 
-            for (let x = 1; x < 49; x++) {
-                for (let y = 1; y < 49; y++) {
+            for (let x = 3; x < 46; x++) {
+                for (let y = 3; y < 46; y++) {
                     let structures = _.filter(room.lookForAtArea(LOOK_STRUCTURES, y, x, y + freeRange, x + freeRange, true), (s) => s.structureType != STRUCTURE_ROAD);
                     if (structures.length > 0) continue;
 
