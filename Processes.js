b9e2cd67460c505cdb7efc9734537e9983ba2a35
Daniel Reid <danielselareid40@gmail.com>
2017-12-09 19:03:39 +0700

    temporarily disabled working scouts cause I don't lnow how to get them to stop going into the novice area

--- Processes.js
+++ Processes.js
@@ -704,7 +704,7 @@ return;
     },
 
     scout: {
-        run: function (Memory_it) {
+        run: function (Memory_it) {return {response: 'idle', time: Game.time + 101};
             var Memory = global.Mem.p[Memory_it];
 
             var room = Game.rooms[Memory.rmN];
